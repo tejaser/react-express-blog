189867a7faf4f64411aebde7a49298a34ed2a321
Tejas Pancholi <tejas.er@outlook.com>
2016-04-04 08:56:53 +0530

    created server configuration

--- app.js
+++ app.js
@@ -1,18 +1,28 @@
 var express = require('express');
+var swig = require('swig');
 var path = require('path');
-var favicon = require('serve-favicon');
+// var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
+// Routes defination
+var index = require('./routes/index');
+var api = require('./routes/api');
+var admin = require('./routes/admin');
+var posts = require('./routes/posts');
+var projects = require('./routes/projects');
 
 var app = express();
 
+// declaring the port from environment
+app.set('port', (process.env.PORT || 8080));
+
 // view engine setup
+var swig = new swig.Swig();
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.engine('html', swig.renderFile);
+app.set('view engine', 'html');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -22,8 +32,12 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
-app.use('/users', users);
+//Initialize the routes
+app.use('/', index);
+app.use('/api', api);
+app.use('/admin', admin);
+app.use('/posts', posts);
+app.use('/projects', projects);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -56,5 +70,11 @@ app.use(function(err, req, res, next) {
   });
 });
 
+// log enabling us to know which port we are listening to
+
+app.listen(app.get('port'), function() {
+    console.log("Listening to", app.get('port'));
+
+});
 
 module.exports = app;
